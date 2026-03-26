03548449e0803417390ed790d55dbcfafb16171c
shruthisugumar <ss2864@kent.ac.uk>
2026-03-26 18:52:43 +0000

    load_questions

--- one_direction_song_quiz/quiz.py
+++ one_direction_song_quiz/quiz.py
@@ -5,4 +5,16 @@ import question import Question
 class QuizGame:
     def __init__(self, filename):
 
-    
+    self.filename = filename
+    self.questions = []
+    self.score = 0 
+    self.curent_question_index = 0
+
+    def load_questions(self):
+        with open(self.filename, "r") as file:
+            
+            for line in file:
+                song, question, options, answer = line.strip().split("|")
+                options_list = options.split(",")
+                q = Question(song, question, options_list, answer)
+                self.questions.append(q)
