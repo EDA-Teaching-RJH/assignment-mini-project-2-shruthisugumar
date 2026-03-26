bd8609bc2469077fec76e1a1f15b287c7002c1cf
shruthisugumar <ss2864@kent.ac.uk>
2026-03-26 18:47:08 +0000

    def init

---
+++ one_direction_song_quiz/question.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, song, question, options, answer):
+
+        self.song = song
+        self.question = question
+        self.options = options
+        self.answer = answer
