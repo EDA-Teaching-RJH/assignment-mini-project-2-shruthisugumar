bd18bb1260908d4734062a066f4bbc71fe9dd8d5
shruthisugumar <ss2864@kent.ac.uk>
2026-03-26 19:02:51 +0000

    check answer and start quiz

--- one_direction_song_quiz/quiz.py
+++ one_direction_song_quiz/quiz.py
@@ -18,3 +18,18 @@ class QuizGame:
                 options_list = options.split(",")
                 q = Question(song, question, options_list, answer)
                 self.questions.append(q)
+
+    def check_answer(self, user_answer, correct_answer):
+        pattern = r"^[abcABC]$"
+        if not re.match(pattern,user_answer):
+            print("Enter a, b or c")
+            return False
+        return user_answer.lower() == correct_answer
+    
+    def start_quiz(self):
+        random.shuffle(self.questions)
+        self.score = 0 
+        self.current_question_index = 0
+        self ask_questions()
+
+    
